66930309445ad2d74689e3a3a0dc42e932e2d4d0
Florian <florianmtddev@gmail.com>
2025-09-17 15:07:09 +0200

    alphaMap edges bug fix and added color attribute to particuls

--- src/script.js
+++ src/script.js
@@ -18,6 +18,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
  * Particles
@@ -25,13 +26,15 @@ const textureLoader = new THREE.TextureLoader()
 
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry(1, 32, 32)
-const count = 500
+const count = 5000
 
 const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
 for(let i = 0; i < count * 3; i++)
 {
     positions[i] = (Math.random() - 0.5) * 10
+    colors[i]= Math.random()
 }
 
 particlesGeometry.setAttribute(
@@ -39,10 +42,24 @@ particlesGeometry.setAttribute(
     new THREE.BufferAttribute(positions, 3)
 )
 
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
 // Material
 const particlesMaterial = new THREE.PointsMaterial
-particlesMaterial.size = 0.02
+particlesMaterial.size = 0.1
 particlesMaterial.sizeAttenuation = true
+particlesMaterial.color = new THREE.Color('#ff88cc')
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// Alpha Fixes
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+// particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
