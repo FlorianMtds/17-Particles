fcf6155265c87dab575d1f9710b59e060e4fba46
Florian <florianmtddev@gmail.com>
2025-09-17 15:10:08 +0200

    particles animation

--- src/script.js
+++ src/script.js
@@ -118,6 +118,9 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update particles
+    particles.position.y = - elapsedTime * 0.2
+
     // Update controls
     controls.update()
 
